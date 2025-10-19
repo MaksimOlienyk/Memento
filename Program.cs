417be40d16b8d6ef00050b8b255913e105f4fba3
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-10-19 19:05:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿class Memento { public string State; public Memento(string s) { State = s; } }
+class Originator
+{
+    public string State;
+    public Memento Save() => new(State);
+    public void Restore(Memento m) => State = m.State;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var o = new Originator { State = "A" };
+        var m = o.Save();
+        o.State = "B"; o.Restore(m);
+        Console.WriteLine(o.State);
+    }
+}
